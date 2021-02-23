4e09a9e99ad5f0d6eddc5e3c15a93a98073ec8a2
Vladislav <vlad@grokhotov.ru>
2021-02-23 12:28:22 +0300

    express init

--- packages/jukboks-backend/src/index.ts
+++ packages/jukboks-backend/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello");
+import express from "express";
+
+// const express = require( "express" );
+const app = express();
+const port = 8080; // default port to listen
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, () => {
+    console.log( `server started at http://localhost:${ port }` );
+} );
+
