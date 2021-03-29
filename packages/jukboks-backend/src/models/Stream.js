1236e83b8495b352d6a9b50a9f6910037759653b
Danil Beltyukov <beltyukovdanil@gmail.com>
2021-03-29 21:09:36 +0300

    playlist array -> one

--- packages/jukboks-backend/src/models/Stream.js
+++ packages/jukboks-backend/src/models/Stream.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const StreamSchema = new mongoose.Schema({
   id: mongoose.Types.ObjectId,
   author: { type: Schema.Types.ObjectId, ref: 'User' }, // author <-> User
-  playlist: [{ type: mongoose.Types.ObjectId, ref: 'Playlist' }], // Stream.playlistId <-> Playlist.streamId
+  playlist: { type: mongoose.Types.ObjectId, ref: 'Playlist' }, // Stream.playlistId <-> Playlist.streamId
 });
 
 const Stream = mongoose.model('Stream', StreamSchema);
