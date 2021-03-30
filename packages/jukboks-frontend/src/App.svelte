b1020f856a1aedb19493de1efedc550436c9ed8a
Vladislav <vlad@grokhotov.ru>
2021-03-30 05:23:55 +0300

    implement main parts of main page

--- packages/jukboks-frontend/src/App.svelte
+++ packages/jukboks-frontend/src/App.svelte
@@ -1,21 +1,34 @@
 <script>
   let background = "/img/backgrounds/mainPage.png";
   let vectorSvg = "/img/backgrounds/mainPageVector.svg";
+  let intro = "img/stuff/mainPageIntro.png";
 </script>
 
-<div class='outerMain' style="background: url({background}); background-size: 100% 100%;">
-  <div class='innerMain' style="background: url({vectorSvg}); background-size: 100% 100%;">
+<div
+  class="outerMain"
+  style="background: url({background}); background-size: cover; background-position: center;"
+>
+  <div
+    class="innerMain"
+    style="background: url({vectorSvg}); background-size: 100% 100%;"
+  >
     <h1>jukboks</h1>
   </div>
 </div>
 
-<div class='outerSecondary'>
-  <div class='innerSecondary'>
+<div class="outerSecondary">
+  <h2 id="first">For artists, DJ’s, creators</h2>
+  <h2 id="second">by software engeneers</h2>
+  <img src={intro} alt="intro" />
+  <h1>Create live streams</h1>
+  <div class="preview">
+    <h1>Video preview</h1>
   </div>
+  <h1>Get reactions</h1>
+  <h1>Promote your music</h1>
 </div>
 
 <style>
-
   .outerMain {
     width: 100vw;
     height: 100vh;
@@ -23,14 +36,12 @@
     margin: 0px;
     display: flex;
     justify-content: center; /*Центрирование по горизонтали*/
-    align-items: center;     /*Центрирование по вертикали */
+    align-items: center; /*Центрирование по вертикали */
   }
 
   .innerMain {
     width: 450px;
     height: 450px;
-    /* background: url("/img/backgrounds/mainPageVector.svg"); */
-    /* background-size: 100% 100%; */
     z-index: 10;
     justify-content: center;
     display: flex;
@@ -38,23 +49,60 @@
   }
 
   .innerMain h1 {
-    font-style: normal;
-    font-weight: normal;
     font-size: 72px;
     line-height: 81px;
-    color: #FFFFFF;
+    color: #ffffff;
   }
 
   .outerSecondary {
     width: 100vw;
-    height: 200vh;
-    background: linear-gradient(180deg, #B9669F 0%, #9ADFF2 100%);;
+    height: auto;
+    background: linear-gradient(180deg, #b9669f 0%, #9adff2 100%);
+    display: flex;
+    flex-direction: column;
   }
+
+  .outerSecondary h2 {
+    font-size: 36px;
+    line-height: 40px;
+    color: #000000;
+    margin: 0vh 12.5vw;
+  }
+
+  .outerSecondary h2[id="first"] {
+    margin-top: 10vh;
+  }
+
+  .outerSecondary h2[id="second"] {
+    margin-top: 5vh;
+    align-self: flex-end;
+    text-align: right;
+  }
+
+  .outerSecondary img {
+    margin: 1.5vh 12.5vw 8.5vh;
+    height: 75vh;
+  }
+
+  .outerSecondary h1 {
+    margin: 1.5vh 12.5vw 0vh;
+    font-size: 72px;
+    line-height: 81px;
+    color: #000000;
+  }
+
+  .preview {
+    margin: 5vh 12.5vw 4.5vh;
+    height: 50vh;
+    text-align: center;
+    background-color: aqua;
+  }
+
+
 
   /* @media (min-width: 640px) {
     main {
       max-width: none;
     }
   } */
-  
 </style>
