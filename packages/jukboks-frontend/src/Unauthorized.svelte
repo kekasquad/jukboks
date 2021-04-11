37d4ce19efa0f4c08cf0456c0bb880f974e54861
Vladislav <vlad@grokhotov.ru>
2021-04-11 19:45:04 +0300

    add unauthorized screen

---
+++ packages/jukboks-frontend/src/Unauthorized.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { Link } from "svelte-routing";
+  // function signIn() {
+  //   navigate("/signin", { replace: true });
+  // }
+</script>
+
+<div class="outer">
+  <h1>You are not Unauthorized :(</h1>
+  <h1>To continue you should <Link to="/signin">login</Link></h1>
+</div>
+
+<style>
+  .outer {
+    width: 100%;
+    height: 100vh;
+    padding: 0px;
+    margin: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
