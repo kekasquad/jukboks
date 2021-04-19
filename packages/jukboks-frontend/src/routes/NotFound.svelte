ba282c1b8ebf08b78c704f994226446296e54a4a
Vladislav <vlad@grokhotov.ru>
2021-04-19 20:31:20 +0300

    add not found page

---
+++ packages/jukboks-frontend/src/routes/NotFound.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Link } from 'svelte-navigator';
+</script>
+
+<div class="outer">
+  <h1>404, not found :(</h1>
+  <h1><Link to="/">Main page</Link></h1>
+</div>
+
+<style>
+  .outer {
+    width: 100%;
+    height: 100vh;
+    padding: 0px;
+    margin: 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
