d114ba3eb4d35c855a693a2375b69131f5ec544a
Vladislav <vlad@grokhotov.ru>
2021-04-20 18:13:56 +0300

    delete console log

--- packages/jukboks-backend/src/soundcloud/index.js
+++ packages/jukboks-backend/src/soundcloud/index.js
@@ -12,7 +12,6 @@ const client = got.extend({
 
 const parseMetadataByUrl = async (url) => {
   const resp = await client.get(url);
-  console.log(url); //Don't print anything
   const $ = cheerio.load(resp.body);
 
   const nodes = $('meta');
