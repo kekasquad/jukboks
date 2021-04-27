440b4e3a2e9707ae4ac9882750a5d20114679372
Vladislav <vlad@grokhotov.ru>
2021-04-27 03:28:29 +0300

    try to get it work

--- packages/jukboks-frontend/src/routes/Soundcloud.svelte
+++ packages/jukboks-frontend/src/routes/Soundcloud.svelte
@@ -2,11 +2,10 @@
   import { onMount } from 'svelte';
   import { SC } from '../sc/api';
   export let url = 'https://soundcloud.com/lustry/i-fell-into-your-brown-eyes';
-  export let autoplay = true;
+  // export let autoplay = true;
 
   let widget;
   let sc;
-  let title;
 
   // functions
   let toggle;
@@ -26,9 +25,8 @@
 
     sc.bind(SC.Widget.Events.PLAY, () => {
       sc.getCurrentSound((s) => {
-        title = s.title;
         console.log(`PLAYING`);
-        console.dir(s);
+        // console.dir(s);
       });
     });
 
@@ -36,22 +34,16 @@
   });
 </script>
 
-<div>
-  <h1>SoundCloud test page</h1>
-  {#if title}
-    <h2>{title}</h2>
-  {/if}
-  <button on:click={toggle}>Play/Pause</button>
-  <iframe
-    id="widget"
-    title="SoundCloud Widget"
-    bind:this={widget}
-    src="https://w.soundcloud.com/player/?url={url}"
-    allow={autoplay ? 'autoplay' : null}
-    height={5000}
-  />
-</div>
+<button on:click={toggle}>Play/Pause</button>
+<iframe
+  id="widget"
+  title="SoundCloud Widget"
+  bind:this={widget}
+  src="https://w.soundcloud.com/player/?url={url}&auto_play=true"
+  allow="autoplay"
+/>
 
+<!-- allow={autoplay ? 'autoplay;' : null} -->
 <style>
   #widget {
     z-index: -1;
