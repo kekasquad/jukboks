107f1e55dd9d0fa56b53a25e5b76aac46df4cd9c
Danil <beltyukovdanil@gmail.com>
2021-05-11 18:21:56 +0300

    tweak animations a bit

--- packages/jukboks-frontend/src/components/stream/Emotion.svelte
+++ packages/jukboks-frontend/src/components/stream/Emotion.svelte
@@ -1,6 +1,7 @@
 <script>
   import { createEventDispatcher } from 'svelte';
   import { fade, fly } from 'svelte/transition';
+  import { cubicInOut } from 'svelte/easing';
 
   export let emotion;
 
@@ -14,10 +15,12 @@
 
   setTimeout(function () {
     deleteEmotion(emotion.id);
-  }, 1000);
+  }, 2000 + 400);
 </script>
 
-<div class="emotion" in:fade={{ duration: 1000 }} out:fly={{ duration: 1000, y: -200 }}>{emotion.text}</div>
+<div class="emotion" in:fade={{ duration: 400 }} out:fly={{ duration: 2000, y: -200, easing: cubicInOut }}>
+  {emotion.text}
+</div>
 
 <style>
   .emotion {
